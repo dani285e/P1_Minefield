2510e1257458989d36fa3079576c55918c77cd78
Mathiaskiel <mkiel24@student.aau.dk>
2024-11-22 10:42:59 +0100

    Blast radius næsten færdigt. Mangler kun at lave omkringliggende felter til en anden farve, når de bliver markeret.

--- Blast_Radius.c
+++ Blast_Radius.c
@@ -30,6 +30,7 @@ void radius_of_explosion(int n);
 
 int main(void) {
     int mine_count = MINE_COUNT; // Midlertidig antal
+    int true_flag = TRUE;
     int mine_x;
     int mine_y;
     Mines mines[mine_count]; // Laver en array mines[mine_count] af structen Mines
@@ -40,13 +41,17 @@ int main(void) {
         mines[i].y = mine_y; // Samme som ovenover bare med y
         mines[i].mineID = i; // Samme som ovenover bare med mineID
     }
-    blast_radius(ACTIVE_MINERS, true_false, mine_count, mines); // Kalder blast_radius funktionen
+    blast_radius(ACTIVE_MINERS, &true_flag, mine_count, mines); // Kalder blast_radius funktionen
     return 0;
 }
 
 
 Blast blast_radius(int active_miners, int *true_false, int mine_count, Mines * mines) {
-    Blast blast[mine_count * 9]; // Minecount er 9, da arrayet omkring bomben er 3 x 3
+    Blast *blast = malloc(sizeof(Blast) * mine_count * 9);
+    if (blast == NULL) {
+        printf("Failed to allocate memory");
+        exit(EXIT_FAILURE);
+    }
     if (active_miners > 1) {
         // Funktion der er aktiv, hvis der er 2 eller flere deminers
          *true_false = TRUE;
@@ -55,29 +60,32 @@ Blast blast_radius(int active_miners, int *true_false, int mine_count, Mines * m
         *true_false = FALSE;
     }
     int blast_index = 0;  // Blast index er hvilket felt den er nået til. Den starter i felt 0
-    while(*true_false == 1) { // Imens der er mere end 1 deminer
+    while(*true_false == TRUE) { // Imens der er mere end 1 deminer
         for (int i = 0; i < mine_count; i++) {   // Den tæller, hvor mange miner der er. Den starter ved 0 og tæller op
             for (int j = 0; j < 9; j++) { // Den tæller, hvor mange felter i arrayet omrking minen, som skal kategoriseres som radius
-                blast[i] = add_koordinat(mines[i].y, mines[i].x, mines[i].mineID);
-                blast[blast_index] = add_koordinat(mines[i].y--, mines[i].x, mines[i].mineID); // Under minen
+                int temporary_y = mines[i].y; // Tilføjer temporary values for ikke at ændre i den originale struct
+                int temporary_x = mines[i].x;
+                blast[i] = add_koordinat(temporary_y, temporary_x, mines[i].mineID);
+                blast[blast_index] = add_koordinat(temporary_y--, temporary_x, mines[i].mineID); // Under minen
                 blast_index++; // Tæller op for hver gang den har været i en bestemt del af arrayet, så den ikke kategoriserer det samme felt 2 gange
-                blast[blast_index] = add_koordinat(mines[i].y--, mines[i].x++, mines[i].mineID); // Skråt for under minen
+                blast[blast_index] = add_koordinat(temporary_y--, temporary_x++, mines[i].mineID); // Skråt for under minen
                 blast_index++;
-                blast[blast_index] = add_koordinat(mines[i].y, mines[i].x++, mines[i].mineID); // Til højre for minen
+                blast[blast_index] = add_koordinat(temporary_y, temporary_x++, mines[i].mineID); // Til højre for minen
                 blast_index++;
-                blast[blast_index] = add_koordinat(mines[i].y++, mines[i].x++, mines[i].mineID); // Højre skrå hjørne
+                blast[blast_index] = add_koordinat(temporary_y++, temporary_x++, mines[i].mineID); // Højre skrå hjørne
                 blast_index++;
-                blast[blast_index] = add_koordinat(mines[i].y++, mines[i].x, mines[i].mineID); // Over minen
+                blast[blast_index] = add_koordinat(temporary_y++, temporary_x, mines[i].mineID); // Over minen
                 blast_index++;
-                blast[blast_index] = add_koordinat(mines[i].y++, mines[i].x--, mines[i].mineID); // Venstre skrå hjørne
+                blast[blast_index] = add_koordinat(temporary_y++, temporary_x--, mines[i].mineID); // Venstre skrå hjørne
                 blast_index++;
-                blast[blast_index] = add_koordinat(mines[i].y, mines[i].x--, mines[i].mineID); // Til venstre for minen
+                blast[blast_index] = add_koordinat(temporary_y, temporary_x--, mines[i].mineID); // Til venstre for minen
                 blast_index++;
-                blast[blast_index] = add_koordinat(mines[i].y--, mines[i].x--, mines[i].mineID); // Venstre nederste hjørne
+                blast[blast_index] = add_koordinat(temporary_y--, temporary_x--, mines[i].mineID); // Venstre nederste hjørne
             }
         }
     }
-
+    free(blast);
+    return blast[0];
 }
 Blast add_koordinat(int y, int x, int id) {
     Blast blast; // Definerer en blast ud fra structen Blast
@@ -86,7 +94,3 @@ Blast add_koordinat(int y, int x, int id) {
     blast.mineID = id; // Tildeler blast mineID i structen Blast
     return blast;
 }
-/* void radius_of_explosion(int n) {
-    pow((2 * BLAST + 1), 2);
-    return 0;
-}     */ // Hvis man vil lave det med flere miner, så skal denne funktion bruges
