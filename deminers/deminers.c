4c6f1155f8aa9c834bbe340b2a6796d8cea2c7a3
Daniel7000dk <daniel.alberg8@gmail.com>
2024-11-27 10:33:29 +0100

    made it run. not testet for if all work

--- deminers/deminers.c
+++ deminers/deminers.c
@@ -49,30 +49,6 @@ int deminer() { //this is a replacement for the fake int main(void) function tha
     return 0;
 }
 
-// Funktion der genererer en arena
-void createMap(int* mapSize, char** map) {
-    char* cell; // Cell er en pointer til en char
-    int mapSizetemp = (rand()% 20) + 10;;
-    *mapSize = mapSizetemp;
-    //printf("Please enter a map size\n");
-    //scanf("%d", &mapSizetemp);
-    printf("Map size is: %d\n", mapSizetemp);
-    *map = (char*)malloc(mapSizetemp*mapSizetemp*sizeof(char));
-
-    for (int y = 0; y < mapSizetemp; y++) { // Nested for loop der kører igennem alle pladser i 2D arrayet
-        for (int x = 0; x < mapSizetemp; x++) {
-            int outcome = (rand() % MINES) + 1;
-            cell = getCell(*map, mapSizetemp, y, x); // Sætter cell pointeren lig med den pointer vi får tilbage af getCell funktionen
-            if(outcome == 1) {
-                *cell = 'M';
-            } else if (outcome > 1 && outcome < 5) {
-                *cell = 'X';
-            } else {
-                *cell = 'O';
-            }
-        }
-    }
-}
 
 void create_temproute(int mapSize, char** temp_route) {
     //char* cell; // Cell er en pointer til en char
@@ -119,9 +95,9 @@ void create_elevation_map(int mapSize, char** elevation_map) {
     for (int y = 0; y < mapSize; y++) {
         // Nested for loop der kører igennem alle pladser i 2D arrayet
         for (int x = 0; x < mapSize; x++) {
-            char *cell = getCell(elevation_map, mapSize, y, x);
+            char *cell = getCell(*elevation_map, mapSize, y, x);
             if ( *cell == HILL_SYMBOL) {
-                upper_left = *getCell(elevation_map, mapSize, y-1, x-1);
+                upper_left = getCell(*elevation_map, mapSize, y-1, x-1);
                 *upper_left = LESS_ELEVATION_SYMBOL; //top left corner
                 elevation_map[y-1][x]   = LESS_ELEVATION_SYMBOL;
                 elevation_map[y-1][x+1] = LESS_ELEVATION_SYMBOL;
@@ -137,62 +113,6 @@ void create_elevation_map(int mapSize, char** elevation_map) {
 
 }
 
-char* getCell(char* map, int mapSize, int y, int x) { // getCell funktionen der returnerer en pointer til en char
-    char* cell = map + mapSize * x + y;
-    return cell;
-}
-
-void printMap(char* map, int mapSize) { // printMap funktionen udskriver alle elementer i 2D arrayet
-
-    int mineCounter = 0;
-    for (int y = 0; y < mapSize; y++) {
-        for (int x = 0; x < mapSize; x++) {
-            if (*getCell(map, mapSize, y, x) == 'M') {
-                red();
-                printf("%2c", *getCell(map, mapSize, y, x));
-                reset();
-                mineCounter++;
-            } else if (*getCell(map, mapSize, y, x) == 'X') {
-                yellow();
-                printf("%2c", *getCell(map, mapSize, y, x));
-                reset();
-            } else {
-                green();
-                printf("%2c", *getCell(map, mapSize, y, x)); // Udskriver den værdi der ligger på hver plads i 2D arrayet
-                reset();
-            }
-        }
-        printf("\n");
-    }
-    printf("\n\n");
-    printf("There is a total of %d mines there needs to be cleared\n", mineCounter);
-    printf("They are located at the following points\n");
-    int counter = 0;
-    for(int y = 0; y < mapSize; y++) {
-        for (int x = 0; x < mapSize; x++) {
-            if (*getCell(map, mapSize, y, x) == 'M') {
-                counter = counter + 1;
-                printf("%-2d Mine: X = %-2d, Y = %d\n", counter, x, y);
-            }
-        }
-    }
-}
-
-void red() {
-    printf("\033[0;31m");
-}
-
-void green() {
-    printf("\033[0;32m");
-}
-
-void reset() {
-    printf("\033[0m");
-}
-
-void yellow() {
-    printf("\033[0;33m");
-}
 
 int get_deminers(char* map, int mapSize) {
     int mineCounter = 0;
@@ -222,9 +142,9 @@ int is_mine(char* map, int mapSize, int y, int x) {
 
 
 double cell_time(char* map, int mapSize, char* route, char* elevation_map, int y, int x) {
-    double incline;
     //check the route map if the cell is part of the path
     if (*getCell(route, mapSize, y, x) == PATH_SYMBOL) {
+        double incline;
         //creates an elevation coefficient from the elevation map
         switch (*getCell(elevation_map, mapSize, y, x)) {
             case HILL_SYMBOL: incline = 1.5;
