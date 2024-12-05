513c1b9a56f001a060fcad0b14e9511a8289af0b
Daniel7000dk <daniel.alberg8@gmail.com>
2024-12-05 15:28:25 +0100

    Added weight to BFT

--- src/path/path.h
+++ src/path/path.h
@@ -18,11 +18,11 @@ int is_empty(Queue* queue);
 void free_queue(Queue* queue);
 
 void find_shortest_path (int mapSize, mapPoint* map, int amount_of_deminers, Deminer* deminers);
-void find_closest_mine(int* shortest_distance_x, int* shortest_distance_y, int* shortest_distance, int mapSize, mapPoint* map,
-                        int whose_turn, Deminer* deminers, int* amount_of_mines, int** path, int* path_length);
+void find_closest_mine(int* shortest_distance_x, int* shortest_distance_y, int* shortest_distance, int* shortest_distance_weight, int mapSize, mapPoint* map,
+                        int whose_turn, Deminer* deminers, int* amount_of_mines, int** path, int* path_length, int* weight);
 
 
-int bfs_find_distance(int mapsize, mapPoint* map, int start_x, int start_y, int target_x, int target_y, int** path, int* path_length);
+int bfs_find_distance(int mapsize, mapPoint* map, int start_x, int start_y, int target_x, int target_y, int** path, int* path_length, int* weight);
 void reset_path(int** path, int mapsize);
 void print_path(int shortest_distance, int** path, int mapSize, mapPoint* map);
 void update_deminer(Deminer* deminers, int* whose_turn, int shortest_distance, int shortest_distance_x, int shortest_distance_y, int amount_of_deminers);
